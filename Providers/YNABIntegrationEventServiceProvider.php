74c7cc5d3c90b9cbd3b07573c08ed49ae8469643
AndrewClyne <andrew@clyne.eu>
2018-07-18 14:44:10 +0100

    Removed the sidebar. Actually registered the event

--- Providers/YNABIntegrationEventServiceProvider.php
+++ Providers/YNABIntegrationEventServiceProvider.php
@@ -4,12 +4,13 @@ namespace Modules\YNABIntegration\Providers;
 
 use App\Events\PaymentWasCreated;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider;
+use Modules\YNABIntegration\Events\Handlers\YNABIntegrationPaymentWasCreated;
 
 class YNABIntegrationEventServiceProvider extends EventServiceProvider
 {
     protected $listen = [
         PaymentWasCreated::class => [
-
+            YNABIntegrationPaymentWasCreated::class
         ]
     ];
 }
